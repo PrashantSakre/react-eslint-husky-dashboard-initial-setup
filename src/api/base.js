de114bf3d7f86ca3f6b2fc978856c9c432f16be9
Prashant <42613882+PrashantSakre@users.noreply.github.com>
2024-07-08 21:06:09 +0530

    fix: remove/add env, craco

--- src/api/base.js
+++ src/api/base.js
@@ -1,9 +1,10 @@
+/* eslint-disable func-names */
 import axios from 'axios'
 
 // configure an axios instance
 const instance = axios.create({
   // eslint-disable-next-line no-use-before-define
-  baseURL: process.env.REACT_APP_API, // eslint-disable-line no-use-before-define
+  baseURL: process.env.REACT_APP_API || 'http://localhost:3001', // eslint-disable-line no-use-before-define
   headers: {
     'Content-Type': 'application/json',
   },
@@ -13,6 +14,7 @@ instance.interceptors.response.use(
   function (response) {
     return response
   },
+  // eslint-disable-next-line func-names
   function (error) {
     if (error?.response?.status === 401) {
       localStorage.clear()
